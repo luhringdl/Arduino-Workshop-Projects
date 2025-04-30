832b97477ec2bb998d928232b03a18e063d76ec5
luhringdl <luhringdl@gmail.com>
2025-04-30 12:34:01 -0500

    Update and rename sketch_apr26_Project5d.ino to sketch_apr26_Project5c.ino

--- sketch_apr26_Project5c.ino
+++ sketch_apr26_Project5c.ino
@@ -1,6 +1,4 @@
-// Project 5d - Controlling Traffic by Levi Luhring
-
-#include <IRremote.h>  // Add IR remote library
+// Project 5c - Controlling Traffic by Levi Luhring
 
 // Define the pins for buttons and lights
                               //wire color
@@ -17,82 +15,48 @@
 #define pedestrianButton 5    //red
 #define pedestrianLight 6     //brown
 
-#define irReceive 9 //left bump black
-/*      IR GND mid bump         white
-        IR Vs  right bump       gray
-        Ground                  purple */
-
-#define yellowBlinkTime 500         // 0.5 seconds for yellow light blink
-boolean trafficWest = true;         // west = true, east = false
-int flowTime = 10000;               // Time for traffic flow
-int changeDelay = 2000;             // Time between color changes
-
-IRrecv irrecv(irReceive);           // Set up IR receiver
-unsigned long lastIRCode = 0;       // Track last IR code
+#define yellowBlinkTime 500   // 0.5 seconds for yellow light blink
+boolean trafficWest = true;   // west = true, east = false
+int flowTime = 4000;          // Time for traffic flow
+int changeDelay = 2000;       // Time between color changes
 
 void setup() {
+  // Button pins configured as inputs (external pull-up resistors used)
   pinMode(westButton, INPUT);
   pinMode(eastButton, INPUT);
+  pinMode(pedestrianButton, INPUT);
+
+  // Output pins for traffic lights and pedestrian light
   pinMode(westRed, OUTPUT);
   pinMode(westYellow, OUTPUT);
   pinMode(westGreen, OUTPUT);
   pinMode(eastRed, OUTPUT);
   pinMode(eastYellow, OUTPUT);
   pinMode(eastGreen, OUTPUT);
-
-  pinMode(pedestrianButton, INPUT);
   pinMode(pedestrianLight, OUTPUT);
-  digitalWrite(pedestrianLight, LOW);  // Turn off pedestrian light initially
 
-  // Set initial state: west starts green, east red
+  // Start with pedestrian light off
+  digitalWrite(pedestrianLight, LOW);
+
+  // Initial traffic light state: west green, east red
   digitalWrite(westRed, LOW);
   digitalWrite(westYellow, LOW);
   digitalWrite(westGreen, HIGH);
   digitalWrite(eastRed, HIGH);
   digitalWrite(eastYellow, LOW);
   digitalWrite(eastGreen, LOW);
-
-  Serial.begin(9600);    // Start serial for debug
-  irrecv.enableIRIn();   // Start IR receiver
 }
 
 void loop() {
-  if (digitalRead(westButton) == HIGH && !trafficWest) {
-    allowTrafficFlow(true);
+  // Detect button presses (active LOW)
+  if (digitalRead(westButton) == LOW && !trafficWest) {
+    allowTrafficFlow(true);  // Switch to westbound traffic
   }
-  if (digitalRead(eastButton) == HIGH && trafficWest) {
-    allowTrafficFlow(false);
+  if (digitalRead(eastButton) == LOW && trafficWest) {
+    allowTrafficFlow(false); // Switch to eastbound traffic
   }
-  if (digitalRead(pedestrianButton) == HIGH) {
-    allowPedestrianCrossing();
-  }
-
-  // IR Remote Handling
-  if (irrecv.decode()) {
-    unsigned long code = irrecv.decodedIRData.decodedRawData;
-    if (code != 0 && code != lastIRCode) { // Ignore repeated signals
-      lastIRCode = code;
-      // Check which button was pressed
-      switch (code) {
-        case 0xD02B3085: // Button 4 for westbound
-          if (!trafficWest) {
-            Serial.println("IR: Westbound Triggered");
-            allowTrafficFlow(true);
-          }
-          break;
-        case 0xD02E3085: // Button 7 for eastbound
-          if (trafficWest) {
-            Serial.println("IR: Eastbound Triggered");
-            allowTrafficFlow(false);
-          }
-          break;
-        default:
-          Serial.print("IR: Unknown Code ");
-          Serial.println(code, HEX);
-          break;
-      }
-    }
-    irrecv.resume(); // Ready for next IR signal
+  if (digitalRead(pedestrianButton) == LOW) {
+    allowPedestrianCrossing(); // Let pedestrians cross
   }
 }
 
@@ -120,7 +84,7 @@ void blinkYellowLight(int yellowPin) {
 // Function to handle traffic flow switch
 void allowTrafficFlow(bool toWest) {
   trafficWest = toWest;
-  delay(flowTime);  // Allow current traffic to flow
+  delay(flowTime);  // Allow current traffic to flow before switching
 
   if (toWest) {
     changeLightsToRed(eastGreen, eastYellow, eastRed);
@@ -143,14 +107,10 @@ void allowPedestrianCrossing() {
 
   // Turn on pedestrian light
   digitalWrite(pedestrianLight, HIGH);
-
-  // Keep it on for 20 seconds
-  delay(20000);
-
-  // Turn off pedestrian light
+  delay(10000);  // 10 seconds walk time
   digitalWrite(pedestrianLight, LOW);
 
-  // Resume traffic flow in the direction it was before
+  // Resume traffic flow in the previous direction
   if (trafficWest) {
     digitalWrite(westRed, LOW);
     digitalWrite(westGreen, HIGH);
